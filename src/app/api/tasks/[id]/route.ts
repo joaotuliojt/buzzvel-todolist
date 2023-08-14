d3102d070a35237dc2921269111f6a0330fd0acd
João Túlio <joaotuliosoares@hotmail.com>
2023-08-14 00:06:37 -0300

    feat: edit task endpoint

--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -1,5 +1,6 @@
 import { prisma } from "@/services/PrismaClient";
 import { NextResponse } from "next/server";
+import { TaskSchema } from "../schemas";
 
 export async function DELETE(
   req: Request,
@@ -18,6 +19,35 @@ export async function DELETE(
   try {
     await prisma.task.delete({ where: { id: params.id } });
     return NextResponse.json({ message: "Task deleted successfully" });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 400 });
+  }
+}
+
+export async function PUT(
+  req: Request,
+  { params }: { params: { id: string } },
+) {
+  if (!params.id) {
+    NextResponse.json({ error: "Pass taskid" }, { status: 400 });
+  }
+
+  const body = await req.json();
+  const validation = TaskSchema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json({ error: validation.error }, { status: 400 });
+  }
+
+  const data = TaskSchema.parse(body);
+
+  try {
+    const editedTask = await prisma.task.update({
+      where: { id: params.id },
+      data,
+    });
+
+    return NextResponse.json({ data: editedTask }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 400 });
   }
